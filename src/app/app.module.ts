0fd48ff8a8b7b38f50a56fd9a153722fe1c92823
bogdanpasterak <bogdanpasterak@gmail.com>
2020-02-25 12:35:30 +0000

    three next component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,10 @@ import { ForsaleComponent } from './forsale/forsale.component';
 import { HomeandcarComponent } from './homeandcar/homeandcar.component';
 
 import { FormsModule } from '@angular/forms';
-import { AboutComponent } from './about/about.component'
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { CarComponent } from './car/car.component';
+import { ContactComponent } from './contact/contact.component'
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +20,10 @@ import { AboutComponent } from './about/about.component'
     HeaderComponent,
     ForsaleComponent,
     HomeandcarComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent,
+    CarComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
