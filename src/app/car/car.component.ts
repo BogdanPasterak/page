1fb50559040d4b2449f99792a4776ca458e46036
bogdanpasterak <bogdanpasterak@gmail.com>
2020-02-25 21:00:29 +0000

    home and car

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -10,12 +10,12 @@ export class CarComponent implements OnInit {
 
   servlist:Serv[] = [
     { symbol: "fas fa-tools", title: 'Pre-NCT and re-Test service', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
-    { symbol: "fas fa-tools", title: 'Common rail injector test', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
-    { symbol: "fas fa-tools", title: 'Car washing and valleting', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
-    { symbol: "fas fa-tools", title: 'Exhaust Welding & Service', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
-    { symbol: "fas fa-tools", title: 'Light alignment for NCT', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
-    { symbol: "fas fa-tools", title: 'Wheel alignment', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
-    { symbol: "fas fa-tools", title: 'Car servicing', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'}
+    { symbol: "fas fa-car-crash", title: 'Common rail injector test', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
+    { symbol: "fas fa-shower", title: 'Car washing and valleting', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
+    { symbol: "fas fa-smog", title: 'Exhaust Welding & Service', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
+    { symbol: "fas fa-lightbulb", title: 'Light alignment for NCT', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
+    { symbol: "fas fa-truck-monster", title: 'Wheel alignment', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
+    { symbol: "fas fa-car", title: 'Car servicing', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'}
   ]
 
   constructor() { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,11 +9,11 @@ import { Serv } from '../serv';
 export class HomeComponent implements OnInit {
 
   servlist: Serv[] = [
-    { symbol: "fas fa-broom", title: 'Hedge and tree trimming', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
-    { symbol: "fas fa-broom", title: 'Land mowing', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
+    { symbol: "fas fa-tree", title: 'Hedge and tree trimming', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
+    { symbol: "fas fa-cut", title: 'Land mowing', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
     { symbol: "fas fa-broom", title: 'Home and garden maintenance', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
-    { symbol: "fas fa-broom", title: 'House painting', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
-    { symbol: "fas fa-broom", title: 'Pressure washing', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'}
+    { symbol: "fas fa-brush", title: 'House painting', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'},
+    { symbol: "fas fa-shower", title: 'Pressure washing', description: 'Bacon ipsum dolor amet andouille cupim swine pig ribeye, drumstick kielbasa pork doner sausage bacon landjaeger venison.'}
   ]
 
   constructor() { }
