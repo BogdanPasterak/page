930717428c301a9ceca18a91df0a6f1dcaecdbe7
bogdanpasterak <bogdanpasterak@gmail.com>
2020-03-24 22:41:40 +0000

    Create User

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from "@angular/fire/auth";
+
+interface User {
+  email?: string;
+  password?: string;
+}
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: User = {
+    email: 'test@test.com',
+    password: 'test1234'
+  };
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth) { }
 
   ngOnInit(): void {
+  }
+
+  async register() {
+    const user = await this.afAuth.auth.createUserWithEmailAndPassword(
+      this.user.email,
+      this.user.password
+    );
+    console.log(user);
   }
 
 }
